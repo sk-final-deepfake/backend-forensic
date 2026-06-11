bb73469bb9e9f68ff88fbe1489587e0259d96c16
owhat02 <littlebear0420@gmail.com>
2026-06-11 14:45:24 +0900

    style: fix missing import and minor cleanup in CaseController

--- src/main/java/com/example/demo/controller/CaseController.java
+++ src/main/java/com/example/demo/controller/CaseController.java
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
